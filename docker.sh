36767e699f00d5e4e72dbcb1497d0418e2af06c8
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-03-21 09:43:38 +0530

    Delete docker.sh

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt update -y
-apt install -y docker.io
-curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-git clone https://github.com/django-cms/django-cms-quickstart.git
-cd django-cms-quickstart
-docker compose build web
